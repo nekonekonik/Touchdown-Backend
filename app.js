a63e1f6eb5ba8d5a1f0fce3b4c7d42f4becfdd70
Nicholette <nekonekonik@gmail.com>
2016-01-16 23:46:05 -0800

    update

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ var client = require('./client');
 
 app.post('/sms', function(req, res) {
 
-    client.sendTo(process.env.TEST_RCVP_NUMBER, req.body);
+    client.sendTo(process.env.TEST_RCVP_NUMBER, req.body.body);
 
     res.writeHead(200);
 });
