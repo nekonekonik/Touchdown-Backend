cfdd0c5f91139e9e3a323017142231b7812d9f2d
Nicholette <nekonekonik@gmail.com>
2016-01-17 05:02:28 -0800

    try2

--- app.js
+++ app.js
@@ -69,10 +69,10 @@ app.post('/sms', function(req, res) {
     var options = {
       method: 'post',
       body: { To: process.env.TEST_RCVP_NUMBER, From: process.env.TWILIO_NUMBER, Body: req.body.message }, // Javascript object
-      json: true, // Use,If you are sending JSON data
+      //json: true, // Use,If you are sending JSON data
       //url: 'https://touchdownhero.herokuapp.com/test',
       url: 'https://api.twilio.com/2010-04-01/Accounts/' + process.env.TWILIO_ACCOUNT_SID + '/Messages',
-      headers: {'Content-Type': 'application/json'},
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
       authorization : {
         username: process.env.TWILIO_ACCOUNT_SID,
         password: process.env.TWILIO_AUTH_TOKEN
