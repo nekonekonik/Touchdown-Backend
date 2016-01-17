cf629dacc852805b7c9f9d676c5dfe3c531e7326
Nicholette <nekonekonik@gmail.com>
2016-01-16 23:26:42 -0800

    update

--- client.js
+++ client.js
@@ -9,17 +9,17 @@ var twilio = require('twilio');
 var client = new twilio.RestClient(ENV[TWILIO_ACCOUNT_SID], ENV[TWILIO_AUTH_TOKEN]);
 
 client.sendInFiveMin = function() {
-    client.sendTo(ENV[PERSONAL_NUMBER], 'Your driver, Nicholette, is arriving in 5 minutes!');
+    client.sendTo(ENV['PERSONAL_NUMBER'], 'Your driver, Nicholette, is arriving in 5 minutes!');
 };
 
 client.sendWhenReached = function() {
-    client.sendTo(ENV[PERSONAL_NUMBER], 'Your driver, Nicholette, has arrived!');
+    client.sendTo(ENV['PERSONAL_NUMBER'], 'Your driver, Nicholette, has arrived!');
 };
 
 client.sendTo = function(recpNum, msg) {
     client.messages.create({ 
         to: recpNum, 
-        from: ENV[TWILIO_NUMBER], 
+        from: ENV['TWILIO_NUMBER'], 
         body: msg
     }, function(err, message) { 
         console.log(err);
