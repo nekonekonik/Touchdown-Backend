40ae340ab0d4946e9c87177f3ea4f8645292cf6c
Nicholette <nekonekonik@gmail.com>
2016-01-17 03:46:33 -0800

    debug

--- app.js
+++ app.js
@@ -57,17 +57,16 @@ app.delete('/quote/:id', function(req, res) {
 
 var client = require('./client');
 
-app.post('/test', function(req, res) {
-  console.log(req);
-});
-
 app.post('/sms', function(req, res) {
+    console.log(req);
+    console.log(req.body);
+    console.log(req.body.message);
     var options = {
       method: 'post',
       body: { To: process.env.TEST_RCVP_NUMBER, From: process.env.TWILIO_NUMBER, Body: req.body.message }, // Javascript object
       json: true, // Use,If you are sending JSON data
-      url: 'https://touchdownhero.herokuapp.com/test',
-      //url: 'https://api.twilio.com/2010-04-01/Accounts/' + process.env.TWILIO_ACCOUNT_SID + '/Messages',
+      //url: 'https://touchdownhero.herokuapp.com/test',
+      url: 'https://api.twilio.com/2010-04-01/Accounts/' + process.env.TWILIO_ACCOUNT_SID + '/Messages',
       headers: {'Content-Type': 'application/x-www-form-urlencoded'},
       authorization : {
         username: process.env.TWILIO_ACCOUNT_SID,
